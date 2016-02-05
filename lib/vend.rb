9e05b8557a7a45617ac17608ff119a2ef4bfb2ea
jeffgrunewald <jeff@grunewalddesign.com>
2016-02-05 16:34:37 -0500

    second test with initial values

--- lib/vend.rb
+++ lib/vend.rb
@@ -1,8 +1,12 @@
 # Implement the logic of a vending machine that accepts nickels, dimes, and quarters
-# and dispenses cola, chips, and candy.
+# and dispenses cola, chips, and candy. More specific behaviors as described below.
 
 class VendingMachine
     # Has accessors for valid coins and current amount.
     attr_accessor :current_amount
+
+    def initialize()
+        self.current_amount = 0
+    end
 
 end
--- spec/vend_spec.rb
+++ spec/vend_spec.rb
@@ -8,4 +8,8 @@ describe "Vending Machine" do
         is_expected.to respond_to(:current_amount)
     end
 
+    it "should initially set current amount to $0" do
+        expect(vendingMachine.current_amount).to be == 0
+    end
+
 end
