587027a04da8005273e51b6434b9ff72830cb47e
jeffgrunewald <jeff@grunewalddesign.com>
2016-02-05 16:54:07 -0500

    set up all initial class attributes

--- lib/vend.rb
+++ lib/vend.rb
@@ -3,10 +3,17 @@
 
 class VendingMachine
     # Has accessors for valid coins and current amount.
-    attr_accessor :current_amount
+    attr_accessor :current_amount, :current_coins, :nickels, :dimes, :quarters, :cola, :chips, :candy
 
-    def initialize()
+    def initialize
         self.current_amount = 0
+        self.current_coins = []
+        self.nickels = 40
+        self.dimes = 20
+        self.quarters = 4
+        self.cola = {cost: 1.00, count: 20}
+        self.chips = {cost: 0.50, count: 20}
+        self.candy = {cost: 0.65, count: 20}
     end
 
 end
--- spec/vend_spec.rb
+++ spec/vend_spec.rb
@@ -4,12 +4,18 @@ require_relative "../lib/vend"
 describe "Vending Machine" do
     subject(:vendingMachine) {VendingMachine.new}
 
-    it "has an accessor for current amount" do
+    it "has accessors for current amount, valid coins, products sold, and coins entered" do
         is_expected.to respond_to(:current_amount)
+        is_expected.to respond_to(:current_coins)
+        is_expected.to respond_to(:dimes)
+        is_expected.to respond_to(:cola)
     end
 
-    it "should initially set current amount to $0" do
+    it "should initialize attribute values representing a full machine ready to make change" do
         expect(vendingMachine.current_amount).to be == 0
+        expect(vendingMachine.current_coins).to be == []
+        expect(vendingMachine.nickels).to be == 40
+        expect(vendingMachine.candy).to be == {cost: 0.65, count: 20}
     end
 
 end
