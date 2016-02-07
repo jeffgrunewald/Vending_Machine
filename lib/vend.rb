da06ba811622295fc60dc1aaf8bb127fec34daf0
jeffgrunewald <jeff@grunewalddesign.com>
2016-02-07 15:10:09 -0500

    test of insert method

--- lib/vend.rb
+++ lib/vend.rb
@@ -3,17 +3,37 @@
 
 class VendingMachine
     # Has accessors for valid coins and current amount.
-    attr_accessor :current_amount, :current_coins, :nickels, :dimes, :quarters, :cola, :chips, :candy
+    attr_accessor :current_amount, :valid_coins, :invalid_coins, :nickels, :dimes, :quarters, :cola, :chips, :candy
 
     def initialize
         self.current_amount = 0
-        self.current_coins = []
+        self.valid_coins = []
+        self.invalid_coins = []
         self.nickels = 40
         self.dimes = 20
         self.quarters = 4
         self.cola = {cost: 1.00, count: 20}
         self.chips = {cost: 0.50, count: 20}
         self.candy = {cost: 0.65, count: 20}
+    end
+
+    def insert coin
+        case coin
+        when "nickel"
+            self.valid_coins.push(coin)
+            self.current_amount += 0.05
+            self.nickels += 1
+        when "dime"
+            self.valid_coins.push(coin)
+            self.current_amount += 0.10
+            self.dimes += 1
+        when "quarter"
+            self.valid_coins.push(coin)
+            self.current_amount += 0.25
+            self.quarters += 1
+        else
+            puts("Returned " + coin)
+        end
     end
 
 end
--- spec/vend_spec.rb
+++ spec/vend_spec.rb
@@ -6,16 +6,27 @@ describe "Vending Machine" do
 
     it "has accessors for current amount, valid coins, products sold, and coins entered" do
         is_expected.to respond_to(:current_amount)
-        is_expected.to respond_to(:current_coins)
+        is_expected.to respond_to(:valid_coins)
         is_expected.to respond_to(:dimes)
         is_expected.to respond_to(:cola)
     end
 
     it "should initialize attribute values representing a full machine ready to make change" do
         expect(vendingMachine.current_amount).to be == 0
-        expect(vendingMachine.current_coins).to be == []
+        expect(vendingMachine.valid_coins).to be == []
         expect(vendingMachine.nickels).to be == 40
         expect(vendingMachine.candy).to be == {cost: 0.65, count: 20}
+    end
+
+    it "should accept a nickel and reject a penny" do
+        vendingMachine.insert("nickel")
+        expect(vendingMachine.current_amount).to be == 0.05
+        expect(vendingMachine.valid_coins).to be == ["nickel"]
+        expect(vendingMachine.nickels).to be == 41
+        vendingMachine.insert("penny")
+        expect(vendingMachine.current_amount).to be == 0.05
+        expect(vendingMachine.valid_coins).to be == ["nickel"]
+        expect(output) === "Returned penny"
     end
 
 end
